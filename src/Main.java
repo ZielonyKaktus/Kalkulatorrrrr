5ea6f416bbb92e310aeb1c3e1b82c7ad11133735
Darcia <lapras098@gmail.com>
2022-12-14 13:12:22 +0100

    Update

--- src/Main.java
+++ src/Main.java
@@ -1,46 +1,56 @@
 import java.util.Scanner;
 
 public class Main {
+    static int printMenu() {
+        System.out.println("===========================");
+        System.out.println("Wybierz:");
+        System.out.println("1 - dodawanie");
+        System.out.println("2 - odejmowanie");
+        System.out.println("3 - mnożenie");
+        System.out.println("4 - dzielenie");
+        System.out.println("5 - KONIEC");
+        System.out.println("===========================");
+
+        Scanner console = new Scanner(System.in);
+        return console.nextInt();
+    }
+
+    static double[] readInput() {
+        double[] arr = new double[2];
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Podaj a:");
+        arr[0] = scanner.nextDouble();
+        System.out.println("Podaj b:");
+        arr[1] = scanner.nextDouble();
+        return arr;
+    }
+
     public static void main(String[] args) {
-        int a;
-        int b;
-        int condition;
-        int i = 1;
-        while (i < 2) {
-            Scanner console = new Scanner(System.in);
-            System.out.println("Wybierz: 1 - dodawanie, 2 - odejmowanie, 3 - mnożenie, 4 - dzielenie, 5 - KONIEC");
-            condition = console.nextInt();
-            if (condition == 1) {
-                System.out.println();
-                System.out.println("Podaj a:");
-                a = console.nextInt();
-                System.out.println("Podaj b:");
-                b = console.nextInt();
-                System.out.println("Pitagorasss to:" + (a + b));
-            }
-            if (condition == 2) {
-                System.out.println();
-                System.out.println("Podaj a:");
-                a = console.nextInt();
-                System.out.println("Podaj b:");
-                b = console.nextInt();
-                System.out.println("Pitagorasss to:" + (a - b));
-            }
-            if (condition == 3) {
-                System.out.println();
-                System.out.println("Podaj a:");
-                a = console.nextInt();
-                System.out.println("Podaj b:");
-                b = console.nextInt();
-                System.out.println("Pitagorasss to:" + (a * b));
-            }
-            if (condition == 4) {
-                System.out.println();
-                System.out.println("Podaj a:");
-                a = console.nextInt();
-                System.out.println("Podaj b:");
-                b = console.nextInt();
-                System.out.println("Pitagorasss to:" + (a / b));
+
+        while (true) {
+            int condition = printMenu();
+
+            switch (condition) {
+                case 1 -> {
+                    double[] dane = readInput();
+                    System.out.println("a + b = " + (dane[0] + dane[1]));
+                }
+                case 2 -> {
+                    double[] dane = readInput();
+                    System.out.println("a - b = " + (dane[0] - dane[1]));
+                }
+                case 3 -> {
+                    double[] dane = readInput();
+                    System.out.println("a * b = " + (dane[0] * dane[1]));
+                }
+                case 4 -> {
+                    double[] dane = readInput();
+                    System.out.println("a / b = " + (dane[0] / dane[1]));
+                }
+                case 5 -> {
+                    System.out.println("KONIEC");
+                    System.exit(0);
+                }
             }
         }
     }
